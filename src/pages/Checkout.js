c7eba66d3d34ff57842e324a5363eb6da0a4ca3c
Michael Tariao <michael.tariao@medieinstitutet.se>
2022-03-10 23:43:32 +0100

    enhancments in checkout.js

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -7,29 +7,29 @@ function Checkout({checkoutProduct}) {
     <div>
       
       <h1>Checkout</h1>
-      {
-        checkoutProduct.map (car => (
-          <div key={car.id}>
-            <table>
-              <tbody>
-              <tr>
-                  <td><p>Car model</p></td>
-                  <td><p>Price</p></td>
-                  <td><p>Picture</p></td>
-                </tr>
+        {
+          checkoutProduct.map (car => (
+            <div key={car.id}>
+              <table>
+                <tbody>
                 <tr>
-                  <td><p>{car.title}</p></td>
-                  <td><p>{car.price} €</p></td>
-                  <td><img src={car.url} alt='Your soon to be car' /></td>
-                </tr>
-              </tbody>
-            </table>
-            <h2>Total cost:</h2>
-          </div>
-        ))
-  
-      }
-
+                    <td><p>Car model</p></td>
+                    <td><p>Price</p></td>
+                    <td><p>Picture</p></td>
+                  </tr>
+                  <tr>
+                    <td><p>{car.title}</p></td>
+                    <td><p>{car.price} €</p></td>
+                    <td><img src={car.url} alt='Your soon to be car' /></td>
+                  </tr>
+                </tbody>
+              </table>
+              
+            </div>
+          ))
+    
+        }
+      <h2>Total cost:</h2>
     </div>
   )
 }
