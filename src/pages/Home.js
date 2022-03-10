0f8ea8c01b049cec5ddab69d25062f033d5c11c3
Michael Tariao <michael.tariao@medieinstitutet.se>
2022-03-10 23:11:05 +0100

    homestyling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import homeStyle from '../components/styling/homeStyle.module.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className={homeStyle.homeBackground}>Home</div>
   )
 }
 
